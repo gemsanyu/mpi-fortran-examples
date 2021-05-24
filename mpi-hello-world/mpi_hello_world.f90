8997bc17317cde3d74be9a75563e0375eda63734
gemsanyou <git.gemilang.santiyuda@gmail.com>
2021-05-24 09:00:42 +0700

    add-get-host

--- mpi-hello-world/mpi_hello_world.f90
+++ mpi-hello-world/mpi_hello_world.f90
@@ -4,12 +4,13 @@ program mpi_hello_world
   real, parameter :: MASTER_RANK = 0
 
   integer :: process_rank, cluster_size, ierror
+  integer :: hostname_len
   character(32) :: hostname
 
   call MPI_INIT(ierror)
   call MPI_COMM_SIZE(MPI_COMM_WORLD, cluster_size, ierror)
   call MPI_COMM_RANK(MPI_COMM_WORLD, process_rank, ierror)
-  call get_environment_variable('HOSTNAME',hostname)
+  call MPI_GET_PROCESSOR_NAME(hostname, hostname_len, ierror)
 
   print *, "Hello World, I'm Process with Rank:", process_rank, "From Host:", hostname
 

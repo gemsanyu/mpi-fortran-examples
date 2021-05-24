0a96343049f034ac6cafb4648f3bbda60b2e82cf
gemsanyou <git.gemilang.santiyuda@gmail.com>
2021-05-24 07:47:12 +0700

    add-hosts

---
+++ scatter/slurm.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#
+#SBATCH --job-name=mpi_send
+#SBATCH --output=mpi_send_%A.out
+#SBATCH --error=mpi_send_%A.err
+#
+#SBATCH --time=00:01:00
+#SBATCH --nodelist=komputasi03
+
+make clean; make all;
+mpirun -np 4 send;
